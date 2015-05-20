66a4426fafe08dd2646fac3e948c80b1bd806ad4
Matt Havard <matt.havard@idea-evolver.com>
2015-05-20 01:29:14 -0400

    More clear instructions

--- main.js
+++ main.js
@@ -9,17 +9,17 @@
 // user should be able to change these inputs and load a different
 // chart WITHOUT refreshing the page.
 
-// This is the only file you may edit.  You are welcome to create as
-// many new files as you want and modify them, but it is strongly
-// advised that you do not modify index.html or highcharts-custom.js.
-// index.html is pre set up enough that you can complete the test
-// without changing it.  You will of course need to carefully review
-// index.html throughout the test as it contains the dom elements you
-// will need to either manipulate or read values from.  If you do end
-// up modifying index.html, you may not do so to add script tags for
-// external JavaScript libraries like jQuery.  At Idea Evolver, we try
-// to use jQuery as little as possible because it is large to load and
-// has much worse performance than raw JS.
+// This is probably the only file you will edit.  You are welcome to
+// create and modify as many new files as you want, but it is
+// strongly advised that you do not modify index.html or
+// highcharts-custom.js.  index.html is pre set up enough that you can
+// complete the test without changing it.  You will of course need to
+// carefully review index.html throughout the test as it contains the
+// dom elements you will need to either manipulate or read values
+// from.  If you do end up modifying index.html, you may not do so to
+// add script tags for external JavaScript libraries like jQuery.  At
+// Idea Evolver, we try to use jQuery as little as possible because it
+// is large to load and has much worse performance than raw JS.
 
 // The documentation for Compete.com is located here: https://developer.compete.com/documentation/
 
